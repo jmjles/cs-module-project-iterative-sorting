d80d233545bc13016c289592d05d71ee4de2bcab
jmjles <jmjles@gmail.com>
2020-05-18 15:28:33 -0700

    mvp complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,26 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+    for i in range(0, len(arr)):
+        current = i
+        smallest_index = 0
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
-
+        for x in range(smallest_index,len(arr)):
+            if(arr[current] < arr[x]):
+                arr[current], arr[x] = arr[x], arr[current]
+        smallest_index+=1
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    for x in range(0,len(arr) - 1):
+        for y in range(0,len(arr)-1):
+            if (arr[y] > arr[y+1]):
+                arr[y],arr[y+1] = arr[y+1],arr[y]
     return arr
 
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,33 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for x in range(0,len(arr)):
+        current = arr[x]
+        if current == target:
+            return x
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    # let's figure out the total size of the arr
+    left = 0
+    right = len(arr) - 1
 
-    # Your code here
+    while left <= right:
+        # find the midpoint
+        mid = (left + right) // 2
+        # check to see if the midpoint element is our target
+        if arr[mid] == target:
+           return mid
 
-
-    return -1  # not found
+        # check to see if the element should be on the left or right side
+        # of our midpoint
+        if arr[mid] < target:
+            # toss out the left side of the arr
+            # update our `left` index
+            left = mid + 1
+        # otherwise, arr[mid] > target
+        else:
+            # toss out the right side of the arr because the element has to be on the left side
+            right = mid - 1
+    # we didn't find the element
+    return -1
